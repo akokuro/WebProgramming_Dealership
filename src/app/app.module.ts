614f0af85567c289b17cce0ae053885e597920b2
Akoraito Kurai <hanshuey@mail.ru>
2019-09-28 09:42:35 +0400

    Laba2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
