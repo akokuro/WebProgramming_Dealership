4ffd0ee46c6b9b58a3e514053c21bf0512b9e821
Akoraito Kurai <hanshuey@mail.ru>
2019-10-25 20:36:20 +0400

    Laba3 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StartPageComponent } from './start-page/start-page.component';
+import { ModelsComponent } from './models/models.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'models', component: ModelsComponent
+  },
+  {
+    path: 'start', component: StartPageComponent
+  },
+  {
+    path: 'admin', component: AdminComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    StartPageComponent
+    StartPageComponent,
+    ModelsComponent,
+    AdminComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/models/models.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-models',
+  templateUrl: './models.component.html',
+  styleUrls: ['./models.component.less']
+})
+export class ModelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
