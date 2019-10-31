5e4be8f97342d4216bdee94e2a175992fda12487
Akoraito Kurai <hanshuey@mail.ru>
2019-11-01 02:49:47 +0400

    4 laba done

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {CarModel} from './../Classes/CarModel'
-import {HttpService} from './../http.service'
+import { HttpClient} from '@angular/common/http';
+import { CarModel} from './../Classes/CarModel'
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.less'],
-  providers: [HttpService]
+  providers: [HttpClient]
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private httpService: HttpService){}
+  constructor(private http: HttpClient){}
 
-  receivedData: CarModel;
-  done: boolean = false;
-  carModel: CarModel;
+  cars: Array<CarModel> = [];
+
   ngOnInit() {
   }
-  submit(title: string, price: number){
-    this.carModel = new CarModel(title, price)
-    this.httpService.CarModelAdd(this.carModel)
-            .subscribe(
-                (data: CarModel) => {this.receivedData=data; this.done=true;},
-                error => console.log(error)
-            );
+  add(title: string, price: number){
+    const body = {title: title, prise: price};
+    this.http.post('http://localhost:5000/admin/model/add_car/', body).subscribe((data)=>this.get_list());
+  }
+  update(title: string, price: number){
+    const body = {title: title, prise: price};
+    this.http.post('http://localhost:5000/admin/model/update_car/', body).subscribe((data)=>this.get_list());
+  }
+  remove(title: string, price: number){
+    const body = {title: title, prise: price};
+    this.http.post('http://localhost:5000/admin/model/remove_car/', body).subscribe((data)=>this.get_list());
+  }
+  get_list(){
+    this.http.get('http://localhost:5000/get_cars/').subscribe((response: any) => {
+      console.log("response", response);
+      this.cars = [];
+      response.forEach(element => {
+        this.cars.push(element);
+      });
+    });
+    console.log(this.cars);
   }
 }
